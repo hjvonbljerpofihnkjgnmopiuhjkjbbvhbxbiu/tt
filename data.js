e52ca59cea92273ebf9491592464f240d5dbd0e0
hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu <58358771+hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu@users.noreply.github.com>
2024-08-11 09:24:43 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+{
+    "elements": [
+        {"text": "Element 1", "value": 0},
+        {"text": "Element 2", "value": 0},
+        {"text": "Element 3", "value": 0},
+        {"text": "Element 4", "value": 0}
+    ]
+}
