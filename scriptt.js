8790f72ff1ed6943044433d707a23631a07216ff
hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu <58358771+hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu@users.noreply.github.com>
2024-08-11 12:00:32 +0200

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const jsonURL = 'https://raw.githubusercontent.com/hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu/tt/main/data.js'; // Remplace par l'URL de ton fichier JSON
 
+    const discordWebhookURL = 'https://discord.com/api/webhooks/1272132345045975072/GnHJEFxdyzALQwIeuwbB6QEVwODbZzLC-fdvcJsnW1BSCfZsTvS2EbvLj1Fubvw634Kp'; // Remplace par l'URL de ton webhook Discord
+
     // Récupérer les données du fichier JSON
     fetch(jsonURL)
         .then(response => response.json())
@@ -56,6 +58,30 @@ document.addEventListener('DOMContentLoaded', function() {
         data[index].value += change;
         localStorage.setItem('counterData', JSON.stringify(data));
         updateUI();
+        sendDiscordWebhook(data);
+    }
+
+    // Fonction pour envoyer les données au webhook Discord
+    function sendDiscordWebhook(data) {
+        const content = {
+            content: `**Mise à jour des valeurs**:\n${data.map(item => `${item.text}: ${item.value}`).join('\n')}`
+        };
+
+        fetch(discordWebhookURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(content)
+        })
+        .then(response => {
+            if (!response.ok) {
+                console.error('Erreur lors de l\'envoi du webhook Discord');
+            }
+        })
+        .catch(error => {
+            console.error('Erreur lors de la requête webhook Discord:', error);
+        });
     }
 
     // Fonction pour mettre à jour l'interface utilisateur
