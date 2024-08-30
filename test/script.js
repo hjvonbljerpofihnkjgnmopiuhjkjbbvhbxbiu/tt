1538e6db7415a11f7cd6e2d2a4c1124e39d46c20
hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu <58358771+hjvonbljerpofihnkjgnmopiuhjkjbbvhbxbiu@users.noreply.github.com>
2024-08-30 02:08:36 +0200

    Update script.js

--- test/script.js
+++ test/script.js
@@ -205,19 +205,50 @@ document.getElementById('search').addEventListener('input', function() {
 
 
 
-const colorPicker = document.getElementById('colorPicker');
-const hexColor = document.getElementById('hexColor');
-const rgbColor = document.getElementById('rgbColor');
+// Créer un nouvel objet Pickr pour initialiser le color picker
+const pickr = Pickr.create({
+    el: '#color-picker-container',
+    theme: 'classic', // Ou 'monolith', 'nano'
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 1)',
+        'rgba(156, 39, 176, 1)',
+        'rgba(103, 58, 183, 1)',
+        'rgba(63, 81, 181, 1)',
+        'rgba(33, 150, 243, 1)',
+        'rgba(3, 169, 244, 1)',
+        'rgba(0, 188, 212, 1)',
+        'rgba(0, 150, 136, 1)',
+        'rgba(76, 175, 80, 1)',
+        'rgba(139, 195, 74, 1)',
+        'rgba(205, 220, 57, 1)',
+        'rgba(255, 235, 59, 1)',
+        'rgba(255, 193, 7, 1)'
+    ],
 
-colorPicker.addEventListener('input', function () {
-    const color = this.value;
-    hexColor.textContent = color;
-    rgbColor.textContent = hexToRgb(color);
+    components: {
+
+        // Palette de couleurs
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsva: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
 });
 
-function hexToRgb(hex) {
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
-    return `rgb(${r}, ${g}, ${b})`;
-}
+// Écouter les événements pour obtenir les valeurs HEX et RGB
+pickr.on('change', (color, instance) => {
+    const hexColor = color.toHEXA().toString();
+    const rgbColor = color.toRGBA().toString();
+    console.log('HEX:', hexColor);
+    console.log('RGB:', rgbColor);
+});
